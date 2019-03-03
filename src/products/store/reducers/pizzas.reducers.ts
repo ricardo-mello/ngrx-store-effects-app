7eafc4037466d97b136a049c9d8130b9f919c997
Ricardo Mello <ricardo.mello@outlook.com>
2019-03-02 22:00:26 -0300

    creating and registering reducer

---
+++ src/products/store/reducers/pizzas.reducers.ts
@@ -0,0 +1,42 @@
+import * as fromPizzas from '../actions/pizzas.actions';
+import { Pizza } from 'src/products/models/pizza.model';
+
+export interface PizzaState {
+  data: Pizza[];
+  loaded: boolean;
+  loading: boolean;
+}
+
+export const initialState: PizzaState = {
+  data: [],
+  loaded: false,
+  loading: false
+};
+
+export function reducer(state = initialState, action: fromPizzas.PizzasAction) {
+  switch (action.type) {
+    case fromPizzas.LOAD_PIZZAS: {
+      return {
+        ...state,
+        loading: true
+      };
+    }
+
+    case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        loaded: true
+      };
+    }
+
+    case fromPizzas.LOAD_PIZZAS_FAIL: {
+      return {
+        ...state,
+        loading: false,
+        loaded: false
+      };
+    }
+  }
+  return state;
+}
